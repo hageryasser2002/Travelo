319aa15b9079906c050e5b4562ee50ec76374aa5
omarmohamedreda <mrm227420@gmail.com>
2026-01-08 16:14:09 +0200

    Identity

--- Travelo.Domain/obj/Debug/net9.0/Travelo.Domain.AssemblyInfo.cs
+++ Travelo.Domain/obj/Debug/net9.0/Travelo.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Travelo.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b094b35d556059cadd4157f9fea5d226c986c9f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07b47ea699d40c9fec2735eeec40db9a96d1669d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Travelo.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Travelo.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
