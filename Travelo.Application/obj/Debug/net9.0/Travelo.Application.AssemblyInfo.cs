8773c99df05f0f45880855f50a8b98e6d7ffbd22
Ahmed Taher <90255766+ahmedtaher45@users.noreply.github.com>
2026-01-10 12:25:42 +0200

    ..

--- Travelo.Application/obj/Debug/net9.0/Travelo.Application.AssemblyInfo.cs
+++ Travelo.Application/obj/Debug/net9.0/Travelo.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Travelo.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07b47ea699d40c9fec2735eeec40db9a96d1669d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9e2afee424c0796b2bf3d3b0d8df13b09f06236")]
 [assembly: System.Reflection.AssemblyProductAttribute("Travelo.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Travelo.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
