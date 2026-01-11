2486ca53da71d889b74778d4d4cd86241498a277
NoranTarekf953 <ntsm2001@gmail.com>
2026-01-11 12:43:53 +0200

    add generic repository

---
+++ Travelo.Application/Interfaces/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travelo.Application.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        public Task<IEnumerable<T>> GetAll(
+            int?pageNum = null,
+            int? pageSize = null,
+            Func<IQueryable<T>,IQueryable<T>>? include = null);
+        public Task<T?> GetById(
+            int id,
+            Func<IQueryable<T>, IQueryable<T>>? include = null);
+
+        public Task Add(T entity);
+        public void Update(T entity);
+        public void Delete(T entity);
+    }
+}
---
+++ Travelo.Infrastracture/Repositories/GenericRepository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travelo.Application.Interfaces;
+using Travelo.Infrastracture.Contexts;
+
+namespace Travelo.Infrastracture.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext context;
+
+        public GenericRepository(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+        public virtual async Task<IEnumerable<T>> GetAll(
+            int? pageNum = null,
+            int? pageSize = null,
+            Func<IQueryable<T>, IQueryable<T>>? include = null)
+        {
+            IQueryable<T> query = context.Set<T>();
+            if(include is not null)
+            {
+                query = include(query);
+                query = query.AsNoTracking();
+            }
+            if(pageNum.HasValue && pageSize.HasValue)
+            {
+                query = query
+                    .Skip((pageNum.Value - 1) * pageSize.Value)
+                    .Take(pageSize.Value);
+            }
+            return await query.ToListAsync();
+        }
+
+        public virtual async Task<T?> GetById(
+            int id,
+            Func<IQueryable<T>, IQueryable<T>>? include = null)
+        {
+            IQueryable<T> query = context.Set<T>();
+            if (include is not null)
+            {
+                query = include(query);
+            }
+            return await query
+                .FirstOrDefaultAsync(e=> EF.Property<int>(e, "Id") == id);
+        }
+
+        public virtual async Task Add(T entity)
+        {
+            await context.Set<T>().AddAsync(entity);
+        }
+        public virtual void Update(T entity)
+        {
+             context.Set<T>().Update(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            context.Set<T>().Remove(entity);
+            
+        }
+
+    }
+}
